c9b5dff6476b692f2907d225902d775396c63cef
tim <tim@TIM>
2017-12-21 15:32:02 +0100

    apsfm

---
+++ SeniorDating/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SeniorDating.Models;
+
+namespace SeniorDating.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        Logic.UserRepository repost = new Logic.UserRepository();
+
+        [Authorize]
+        public ActionResult Profiles(ApplicationUser model)
+        {
+            var user = repost.GetUser(User.Identity.Name);
+            model.Age = user.Age;
+            model.Name = user.Name;
+            model.About = user.About;
+            model.Gender = user.Gender;
+            model.LookingFor = user.LookingFor;
+
+            return View(model);
+
+        }
+
+    }
+}
