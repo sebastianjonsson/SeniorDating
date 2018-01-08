df52502480aeead08d373114e431436ce3abe6a1
tim <tim@TIM>
2018-01-08 14:26:55 +0100

    fhfghdgfh

--- SeniorDating/Controllers/BaseController.cs
+++ SeniorDating/Controllers/BaseController.cs
@@ -1,4 +1,5 @@
 ﻿using SeniorDating.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SeniorDating.Controllers
@@ -11,5 +12,34 @@ namespace SeniorDating.Controllers
             if (disposing) db.Dispose();
             base.Dispose(disposing);
         }
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (User != null)
+            {
+                var context = new ApplicationDbContext();
+                var username = User.Identity.Name;
+
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var user = context.Users.SingleOrDefault(u => u.UserName == username);
+
+                    if (user != null)
+                    {
+                        string Name = user.Name;
+                        string Id = user.Id;
+
+                        var friendList = user.Friends;
+                        var friendRequests = user.FriendRequests;
+                        
+                        ViewData.Add("Name", Name);
+                        ViewData.Add("Id", Id);
+                        ViewData.Add("friendList", friendList);
+                        ViewData.Add("friendRequests", friendRequests);
+                    }
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
     }
 }
